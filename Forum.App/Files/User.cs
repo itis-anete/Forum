a22b7f503b2542f7682e2a85c44571658ea7f082
Olya Nikolaeva <olya.nikolaeva.2011@bk.ru>
2018-09-10 15:43:22 +0300

    Add a User class

---
+++ Forum.App/Files/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Forum.App
+{
+    public class User
+    {
+        public string Nickname { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public int Password { get; set; }
+
+    }
+}
