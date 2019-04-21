baab0f46ced0c0e782ee2786c935450df1a2f486
Dinopopik <DIValitov.2018@gmail.com>
2019-04-21 22:50:23 +0300

    Check

---
+++ Forum.Web/Controllers/ThemeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forum.Web.Controllers
+{
+    public class ThemeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateTheme(string name, string description)
+        {
+            return Ok();
+        }
+    }
+}
